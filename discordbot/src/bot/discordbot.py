b8e5d96ab8f2b9bbafa3d5fa4fbe3b3a68468244
Jack Chen <jack.chenr@gmail.com>
2017-07-13 22:40:41 -0700

    Change command prefix to '!'

--- discordbot/src/bot/discordbot.py
+++ discordbot/src/bot/discordbot.py
@@ -2,15 +2,17 @@ import discord
 from discord.ext import commands
 import logging
 
+
 logger = logging.getLogger('discord')
 logger.setLevel(logging.INFO)
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 
+
 description = '''Example bot!'''
 
-bot = commands.Bot(command_prefix='?', description=description)
+bot = commands.Bot(command_prefix='!', description=description)
 
 @bot.event
 async def on_ready():
@@ -24,4 +26,5 @@ async def add(left : int, right : int):
     """ Adds two numbers together.""" 
     await bot.say(left + right)
     
-bot.run('token')
+    
+bot.run('MzM1Mjg0MzE0MzA2NjQxOTIy.DEnlPA.xd1-kcQ5LqMdiEJOP3Fl6ReopoE')
