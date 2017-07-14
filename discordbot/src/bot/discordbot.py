fdf41b0eecdf12e427b38caf5c060a3b034fbbbb
Jack Chen <jack.chenr@gmail.com>
2017-07-13 21:37:37 -0700

    Build of basic bot with basic command

---
+++ discordbot/src/bot/discordbot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+import logging
+
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+handler.setFormatter(logging.Formatter('%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
+logger.addHandler(handler)
+
+description = '''Example bot!'''
+
+bot = commands.Bot(command_prefix='?', description=description)
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')
+    
+@bot.command()
+async def add(left : int, right : int):
+    """ Adds two numbers together.""" 
+    await bot.say(left + right)
+    
+bot.run('token')
